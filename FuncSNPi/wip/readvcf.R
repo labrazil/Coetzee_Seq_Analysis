74cde4932ebf33000d009f8cd629bd5578d26612
Houtan@gcimp <hnoushme@usc.edu>
2011-11-11 21:18:21 -0800

    added makePlots argument, cleaned up verbose in FunciSNPPlots, FunciSNPSummary, FunciSNPHeatmap, as well as fixed bug in FunciSNPHeatmap

--- FuncSNPi/wip/readvcf.R
+++ FuncSNPi/wip/readvcf.R
@@ -85,16 +85,20 @@ ServerCheck <- function(primary.server) {
 
 
 FunciSNP <- function(ethno = c("AFR", "ASN", "EUR", "AMR", "ALL"), 
-                     bio.features.loc, snp.regions.file, output.file = "risk.snp", R.squared.cutoff = 0, primary.server) {
+                     bio.features.loc, snp.regions.file, output.file = "risk.snp", R.squared.cutoff = 0, primary.server, makePlots = TRUE) {
     cat("
         ####################################
         ##                                ##
         ##      Welcome to FunciSNP       ##
         ##                                ##
         ####################################
-        args:  snp.regions.file: ", as.character(snp.regions.file), "\n",
-        "      ethno:            ", ethno, "\n",
-        "      bio.features.loc: ", list.files(bio.features.loc, pattern="*.bed$", full.names = FALSE), "\n",
+        args used:\n
+        snp.regions.file:   ", as.character(snp.regions.file), "\n",
+        "       ethno:            ", ethno, "\n",
+        "       bio.features.loc: ", list.files(bio.features.loc, pattern="*.bed$", full.names = FALSE), "\n",
+        "       R.squared.cutoff: ", R.squared.cutoff, "\n",
+        "       Primary Server:   ", primary.server, "\n",
+        "       Make Plots?:      ", makePlots, "\n",
         "\n ### WARNING:: existing vcf.gz files will be removed from ", getwd(),
         "\n ### and new ones will be placed there, press
         ### <Esc> or <Ctrl-C> to cancel if you do not want this to happen after the
@@ -118,10 +122,10 @@ FunciSNP <- function(ethno = c("AFR", "ASN", "EUR", "AMR", "ALL"),
         }
 
         ### Not sure if we want to make this silent or error out if permission is denied. I use plot folder to store graphs.
-        try(dir.create(path="plots"), silent=TRUE) ## directory used to store plots
-        try(dir.create(path="tmp"), silent=TRUE) ## directory used to store temp files
-        try(dir.create(path="log"),silent=TRUE) ## directory used to store log files
-        try(dir.create(path="tables"),silent=TRUE) ## directory used to store summary tables
+        try(dir.create(path="FunciSNP_plots", showWarnings=FALSE), silent=TRUE) ## directory used to store plots
+        try(dir.create(path="FunciSNP_tmp", showWarnings=FALSE), silent=TRUE) ## directory used to store temp files
+        try(dir.create(path="FunciSNP_log", showWarnings=FALSE),silent=TRUE) ## directory used to store log files
+        try(dir.create(path="FunciSNP_tables", showWarnings=FALSE),silent=TRUE) ## directory used to store summary tables
 
         snp.region <- ReadRegionsFile(snp.regions.file)
         snp.names <- snp.region$snp.name
@@ -155,10 +159,15 @@ FunciSNP <- function(ethno = c("AFR", "ASN", "EUR", "AMR", "ALL"),
             file.remove(paste(j, ".vcf.gz.tbi", sep=""))
         }
         write.table(snp.ld.frame, file=paste(output.file, ".txt", sep=""), sep="\t", quote = FALSE, row.names = FALSE)
-        FunciSNPPlot(R.2=R.squared.cutoff, dat = snp.ld.frame)
-        FunciSNPHeatmap(R.2=R.squared.cutoff, dat = snp.ld.frame)
-        FunciSNPSummary(R.2=R.squared.cutoff, dat = snp.ld.frame)
-        cat("FunciSNP is complete!\nSee 'tables' and 'plots' folder for overall results\nSee ", output.file, ".txt for complete results")
+        if(makePlots == TRUE) {
+                FunciSNPSummary(R.2=R.squared.cutoff, dat = snp.ld.frame)
+                FunciSNPPlot(R.2=R.squared.cutoff, dat = snp.ld.frame)
+                FunciSNPHeatmap(R.2=R.squared.cutoff, dat = snp.ld.frame)
+                cat("##########################################\nFunciSNP is complete!\nSee 'FunciSNP_tables' and 'FunciSNP_plots' folder for overall results\nSee", output.file, "for complete results.\n##########################################")
+        } else {
+            FunciSNPSummary(R.2=R.squared.cutoff, dat = snp.ld.frame)
+            cat("##########################################\nFunciSNP is complete!\nSee 'FunciSNP_tables' folder for overall results\nSee", output.file, "for complete results.\n##########################################")
+        }
 }
 
 
@@ -329,12 +338,12 @@ FunciSNPSummary <- function(R.2, dat) {
                                         c("Total", paste("R.squared.cuff.",R.2,sep=""))))
     total.dat <- as.data.frame(total.dat)
     total.dat$Percent <- round((total.dat[,2]/total.dat[,1])*100,2)
-    write.table(total.dat, file="tables/summary.txt", sep="\t", row.names=T, col.names=T, quote=F)
+    write.table(total.dat, file="FunciSNP_tables/summary.txt", sep="\t", row.names=T, col.names=T, quote=F)
 }
 
 FunciSNPPlot <- function(R.2, dat) {
-    cat("Plotting will begin")
-    cat("using R square cut off of ", R.2)
+    cat("\n\nPlotting will begin")
+    cat(" using R square cut off of ", R.2, "\n")
 
 
 
@@ -374,7 +383,7 @@ FunciSNPPlot <- function(R.2, dat) {
         scale_fill_manual(values = c("Yes" = "Red", "No" = "Black")) +
         opts(legend.position = "none", axis.text.y = theme_text(), axis.text.x = theme_text(angle=90), title = paste("riskSNP\nOverlapping: ", bio[i], sep="")) + 
         facet_wrap(~ snp.names.chosen)
-        ggsave(file=paste("plots/",bio[i],"_R2summary_riskSNP.pdf",sep=""))
+        ggsave(file=paste("FunciSNP_plots/",bio[i],"_R2summary_riskSNP.pdf",sep=""))
 
         ## plot r.2 vs. distance values
         p.all.d <- ggplot(tmp, aes(x=R.squared, y=dist, colour=r2, size=factor(r2)))
@@ -388,17 +397,17 @@ FunciSNPPlot <- function(R.2, dat) {
         scale_size_manual(values = c("Yes" = 2, "No" = 1)) +
         opts(legend.position = "none", axis.text.y = theme_text(), axis.text.x = theme_text(angle=90), title = paste("Distance between riskSNP\nand Surrogate SNP\nOverlapping: ", bio[i], sep="")) + 
         facet_wrap(~ snp.names.chosen)
-        ggsave(file=paste("plots/",bio[i],"_R2vsDist_riskSNP.pdf",sep=""))
-        cat("Finished plotting ", i, "/",length(bio))
+        ggsave(file=paste("FunciSNP_plots/",bio[i],"_R2vsDist_riskSNP.pdf",sep=""))
+        cat("Finished plotting ", i, "/",length(bio), "\n")
     }
-    cat("Plots are done, see 'plots' folder in ", getwd())
+    cat("\n\nMaking plots is finished, see 'plots' folder in ", getwd(), "\n\n")
 }
-FunciSNPHeatmap <- function(R.2, all) {
+FunciSNPHeatmap <- function(R.2, dat) {
     cat("Heatmap generation will begin")
-    cat("using R square cut off of ", R.2)
+    cat(" using R square cut off of ", R.2,"\n")
     all.s<-(table( subset(dat,R.squared>=R.2)[,"feature"], subset(dat,R.squared>=R.2)[,"snp.names.chosen"] ))
     all.s <- as.matrix(all.s)
-    png(filename="plots/heatmap.png", bg = "white")
+    png(filename="FunciSNP_plots/heatmap.png", bg = "white")
     heatmap.2(
               all.s,
               na.rm=TRUE,
@@ -422,7 +431,7 @@ FunciSNPHeatmap <- function(R.2, all) {
               #labCol=NULL
               )
     dev.off()
-    cat("Heatmap is complete, see 'plots' folder in ", getwd())
+    cat("\n\nHeatmap plot is complete, see 'FunciSNP_plots' folder in ", getwd(),"\n\n")
 }
 
 
