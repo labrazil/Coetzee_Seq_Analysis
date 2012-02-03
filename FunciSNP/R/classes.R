cd11889203d51191eb78e4f76835d29affba1123
Houtan Noushmehr (laptop-dell) <houtana@gmail.com>
2012-02-03 15:57:35 -0800

    extracted version 0.1.4 so we can modify the same R code

---
+++ FunciSNP/R/classes.R
@@ -0,0 +1,371 @@
+setClass("CorrelatedSNPs",
+         representation(chromosome="integer",
+                        position="integer",
+                        snpid="character",
+                        ref.allele="character",
+                        alt.allele="character",
+                        overlapping.features="GRanges",
+                        ALL.geno="SnpMatrix",
+                        AFR.geno="SnpMatrix",
+                        AMR.geno="SnpMatrix",
+                        ASN.geno="SnpMatrix",
+                        EUR.geno="SnpMatrix",
+                        ALL.overlapping.snps.geno="SnpMatrix",
+                        AFR.overlapping.snps.geno="SnpMatrix",
+                        AMR.overlapping.snps.geno="SnpMatrix",
+                        ASN.overlapping.snps.geno="SnpMatrix",
+                        EUR.overlapping.snps.geno="SnpMatrix",
+                        ALL.R.squared="matrix",
+                        AFR.R.squared="matrix",
+                        AMR.R.squared="matrix",
+                        ASN.R.squared="matrix",
+                        EUR.R.squared="matrix",
+                        ALL.D.prime="matrix",
+                        AFR.D.prime="matrix",
+                        AMR.D.prime="matrix",
+                        ASN.D.prime="matrix",
+                        EUR.D.prime="matrix",
+                        ALL.p.value="list",
+                        AFR.p.value="list",
+                        AMR.p.value="list",
+                        ASN.p.value="list",
+                        EUR.p.value="list"
+                        )
+         )
+
+setClass("TagSNP",
+         representation(chromosome="integer",
+                        position="integer",
+                        snpid="character",
+                        population="character",
+                        ref.allele="character",
+                        alt.allele="character",
+                        overlapping.features="GRanges",
+                        genotype="SnpMatrix",
+                        R.squared.corrsnps="dgCMatrix",
+                        D.prime.corrsnps="dgCMatrix",
+                        correlated.snps="CorrelatedSNPs"
+                        )
+         )
+setClass("TSList",
+         prototype = prototype(elementType = "TagSNP"),
+         contains = "List")
+#setClass("TagSNPList",
+#        contains="TSList",
+#       representaion(R
+
+##TagSNPList <- function(...) {
+##  args <- list(...)
+##  if(length(args) == 1 && is.list(args[[1]]))
+##    args <- args[[1]]
+
+setGeneric("chr", function(x) standardGeneric("chr"))
+setMethod("chr", "TagSNP", function(x) x@chromosome)
+setGeneric("chr<-", function(x, value) standardGeneric("chr<-"))
+setReplaceMethod("chr", "TagSNP", function(x, value) {
+                 x@chromosome <- value
+                 x
+         })
+setGeneric("position", function(x) standardGeneric("position"))
+setMethod("position", "TagSNP", function(x) x@position)
+setGeneric("position<-", function(x, value) standardGeneric("position<-"))
+setReplaceMethod("position", "TagSNP", function(x, value) {
+                 x@position <- value
+                 x
+         })
+setGeneric("snpid", function(x) standardGeneric("snpid"))
+setMethod("snpid", "TagSNP", function(x) x@snpid)
+setGeneric("snpid<-", function(x, value) standardGeneric("snpid<-"))
+setReplaceMethod("snpid", "TagSNP", function(x, value) {
+                 x@snpid <- value
+                 x
+         })
+setGeneric("population", function(x) standardGeneric("population"))
+setMethod("population", "TagSNP", function(x) x@population)
+setGeneric("population<-", function(x, value) standardGeneric("population<-"))
+setReplaceMethod("population", "TagSNP", function(x, value) {
+                 x@population <- value
+                 x
+         })
+setGeneric("ref.allele", function(x) standardGeneric("ref.allele"))
+setMethod("ref.allele", "TagSNP", function(x) x@ref.allele)
+setGeneric("ref.allele<-", function(x, value) standardGeneric("ref.allele<-"))
+setReplaceMethod("ref.allele", "TagSNP", function(x, value) {
+                 x@ref.allele <- value
+                 x
+         })
+setGeneric("alt.allele", function(x) standardGeneric("alt.allele"))
+setMethod("alt.allele", "TagSNP", function(x) x@alt.allele)
+setGeneric("alt.allele<-", function(x, value) standardGeneric("alt.allele<-"))
+setReplaceMethod("alt.allele", "TagSNP", function(x, value) {
+                 x@alt.allele <- value
+                 x
+         })
+setGeneric("overlapping.features", function(x) standardGeneric("overlapping.features"))
+setMethod("overlapping.features", "TagSNP", function(x) x@overlapping.features)
+setGeneric("overlapping.features<-", function(x, value) standardGeneric("overlapping.features<-"))
+setReplaceMethod("overlapping.features", "TagSNP", function(x, value) {
+                 x@overlapping.features <- value
+                 x
+         })
+setGeneric("genotype", function(x) standardGeneric("genotype"))
+setMethod("genotype", "TagSNP", function(x) x@genotype)
+setGeneric("genotype<-", function(x, value) standardGeneric("genotype<-"))
+setReplaceMethod("genotype", "TagSNP", function(x, value) {
+                 x@genotype <- value
+                 x
+         })
+setGeneric("R.squared.corrsnps", function(x) standardGeneric("R.squared.corrsnps"))
+setMethod("R.squared.corrsnps", "TagSNP", function(x) x@R.squared.corrsnps)
+setGeneric("R.squared.corrsnps<-", function(x, value) standardGeneric("R.squared.corrsnps<-"))
+setReplaceMethod("R.squared.corrsnps", "TagSNP", function(x, value) {
+                 x@R.squared.corrsnps <- value
+                 x
+         })
+setGeneric("D.prime.corrsnps", function(x) standardGeneric("D.prime.corrsnps"))
+setMethod("D.prime.corrsnps", "TagSNP", function(x) x@D.prime.corrsnps)
+setGeneric("D.prime.corrsnps<-", function(x, value) standardGeneric("D.prime.corrsnps<-"))
+setReplaceMethod("D.prime.corrsnps", "TagSNP", function(x, value) {
+                 x@D.prime.corrsnps <- value
+                 x
+         })
+setGeneric("correlated.snps", function(x) standardGeneric("correlated.snps"))
+setMethod("correlated.snps", "TagSNP", function(x) x@correlated.snps)
+setGeneric("correlated.snps<-", function(x, value) standardGeneric("correlated.snps<-"))
+setReplaceMethod("correlated.snps", "TagSNP", function(x, value) {
+                 x@correlated.snps <- value
+                 x
+         })
+
+
+
+##### CorrelatedSNPs methods and generics
+setMethod("chr", "CorrelatedSNPs", function(x) x@chromosome)
+setReplaceMethod("chr", "CorrelatedSNPs", function(x, value) {
+                 x@chromosome <- value
+                 x
+         })
+setMethod("position", "CorrelatedSNPs", function(x) x@position)
+setReplaceMethod("position", "CorrelatedSNPs", function(x, value) {
+                 x@position <- value
+                 x
+         })
+setMethod("snpid", "CorrelatedSNPs", function(x) x@snpid)
+setReplaceMethod("snpid", "CorrelatedSNPs", function(x, value) {
+                 x@snpid <- value
+                 x
+         })
+setMethod("ref.allele", "CorrelatedSNPs", function(x) x@ref.allele)
+setReplaceMethod("ref.allele", "CorrelatedSNPs", function(x, value) {
+                 x@ref.allele <- value
+                 x
+         })
+setMethod("alt.allele", "CorrelatedSNPs", function(x) x@alt.allele)
+setReplaceMethod("alt.allele", "CorrelatedSNPs", function(x, value) {
+                 x@alt.allele <- value
+                 x
+         })
+setMethod("overlapping.features", "CorrelatedSNPs", function(x) x@overlapping.features)
+setReplaceMethod("overlapping.features", "CorrelatedSNPs", function(x, value) {
+                 x@overlapping.features <- value
+                 x
+         })
+setGeneric("ALL.geno", function(x) standardGeneric("ALL.geno"))
+setGeneric("ALL.geno<-", function(x, value) standardGeneric("ALL.geno<-"))
+setMethod("ALL.geno", "CorrelatedSNPs", function(x) x@ALL.geno)
+setReplaceMethod("ALL.geno", "CorrelatedSNPs", function(x, value) {
+                 x@ALL.geno <- value
+                 x
+         })
+setGeneric("AFR.geno", function(x) standardGeneric("AFR.geno"))
+setGeneric("AFR.geno<-", function(x, value) standardGeneric("AFR.geno<-"))
+setMethod("AFR.geno", "CorrelatedSNPs", function(x) x@AFR.geno)
+setReplaceMethod("AFR.geno", "CorrelatedSNPs", function(x, value) {
+                 x@AFR.geno <- value
+                 x
+         })
+setGeneric("AMR.geno", function(x) standardGeneric("AMR.geno"))
+setGeneric("AMR.geno<-", function(x, value) standardGeneric("AMR.geno<-"))
+setMethod("AMR.geno", "CorrelatedSNPs", function(x) x@AMR.geno)
+setReplaceMethod("AMR.geno", "CorrelatedSNPs", function(x, value) {
+                 x@AMR.geno <- value
+                 x
+         })
+setGeneric("ASN.geno", function(x) standardGeneric("ASN.geno"))
+setGeneric("ASN.geno<-", function(x, value) standardGeneric("ASN.geno<-"))
+setMethod("ASN.geno", "CorrelatedSNPs", function(x) x@ASN.geno)
+setReplaceMethod("ASN.geno", "CorrelatedSNPs", function(x, value) {
+                 x@ASN.geno <- value
+                 x
+         })
+setGeneric("EUR.geno", function(x) standardGeneric("EUR.geno"))
+setGeneric("EUR.geno<-", function(x, value) standardGeneric("EUR.geno<-"))
+setMethod("EUR.geno", "CorrelatedSNPs", function(x) x@EUR.geno)
+setReplaceMethod("EUR.geno", "CorrelatedSNPs", function(x, value) {
+                 x@EUR.geno <- value
+                 x
+         })
+
+setGeneric("ALL.R.squared", function(x) standardGeneric("ALL.R.squared"))
+setGeneric("ALL.R.squared<-", function(x, value) standardGeneric("ALL.R.squared<-"))
+setMethod("ALL.R.squared", "CorrelatedSNPs", function(x) x@ALL.R.squared)
+setReplaceMethod("ALL.R.squared", "CorrelatedSNPs", function(x, value) {
+                 x@ALL.R.squared <- value
+                 x
+         })
+setGeneric("AFR.R.squared", function(x) standardGeneric("AFR.R.squared"))
+setGeneric("AFR.R.squared<-", function(x, value) standardGeneric("AFR.R.squared<-"))
+setMethod("AFR.R.squared", "CorrelatedSNPs", function(x) x@AFR.R.squared)
+setReplaceMethod("AFR.R.squared", "CorrelatedSNPs", function(x, value) {
+                 x@AFR.R.squared <- value
+                 x
+         })
+setGeneric("AMR.R.squared", function(x) standardGeneric("AMR.R.squared"))
+setGeneric("AMR.R.squared<-", function(x, value) standardGeneric("AMR.R.squared<-"))
+setMethod("AMR.R.squared", "CorrelatedSNPs", function(x) x@AMR.R.squared)
+setReplaceMethod("AMR.R.squared", "CorrelatedSNPs", function(x, value) {
+                 x@AMR.R.squared <- value
+                 x
+         })
+setGeneric("ASN.R.squared", function(x) standardGeneric("ASN.R.squared"))
+setGeneric("ASN.R.squared<-", function(x, value) standardGeneric("ASN.R.squared<-"))
+setMethod("ASN.R.squared", "CorrelatedSNPs", function(x) x@ASN.R.squared)
+setReplaceMethod("ASN.R.squared", "CorrelatedSNPs", function(x, value) {
+                 x@ASN.R.squared <- value
+                 x
+         })
+setGeneric("EUR.R.squared", function(x) standardGeneric("EUR.R.squared"))
+setGeneric("EUR.R.squared<-", function(x, value) standardGeneric("EUR.R.squared<-"))
+setMethod("EUR.R.squared", "CorrelatedSNPs", function(x) x@EUR.R.squared)
+setReplaceMethod("EUR.R.squared", "CorrelatedSNPs", function(x, value) {
+                 x@EUR.R.squared <- value
+                 x
+         })
+setGeneric("ALL.D.prime", function(x) standardGeneric("ALL.D.prime"))
+setGeneric("ALL.D.prime<-", function(x, value) standardGeneric("ALL.D.prime<-"))
+setMethod("ALL.D.prime", "CorrelatedSNPs", function(x) x@ALL.D.prime)
+setReplaceMethod("ALL.D.prime", "CorrelatedSNPs", function(x, value) {
+                 x@ALL.D.prime <- value
+                 x
+         })
+setGeneric("AFR.D.prime", function(x) standardGeneric("AFR.D.prime"))
+setGeneric("AFR.D.prime<-", function(x, value) standardGeneric("AFR.D.prime<-"))
+setMethod("AFR.D.prime", "CorrelatedSNPs", function(x) x@AFR.D.prime)
+setReplaceMethod("AFR.D.prime", "CorrelatedSNPs", function(x, value) {
+                 x@AFR.D.prime <- value
+                 x
+         })
+setGeneric("AMR.D.prime", function(x) standardGeneric("AMR.D.prime"))
+setGeneric("AMR.D.prime<-", function(x, value) standardGeneric("AMR.D.prime<-"))
+setMethod("AMR.D.prime", "CorrelatedSNPs", function(x) x@AMR.D.prime)
+setReplaceMethod("AMR.D.prime", "CorrelatedSNPs", function(x, value) {
+                 x@AMR.D.prime <- value
+                 x
+         })
+setGeneric("ASN.D.prime", function(x) standardGeneric("ASN.D.prime"))
+setGeneric("ASN.D.prime<-", function(x, value) standardGeneric("ASN.D.prime<-"))
+setMethod("ASN.D.prime", "CorrelatedSNPs", function(x) x@ASN.D.prime)
+setReplaceMethod("ASN.D.prime", "CorrelatedSNPs", function(x, value) {
+                 x@ASN.D.prime <- value
+                 x
+         })
+setGeneric("EUR.D.prime", function(x) standardGeneric("EUR.D.prime"))
+setGeneric("EUR.D.prime<-", function(x, value) standardGeneric("EUR.D.prime<-"))
+setMethod("EUR.D.prime", "CorrelatedSNPs", function(x) x@EUR.D.prime)
+setReplaceMethod("EUR.D.prime", "CorrelatedSNPs", function(x, value) {
+                 x@EUR.D.prime <- value
+                 x
+         })
+setGeneric("ALL.p.value", function(x) standardGeneric("ALL.p.value"))
+setGeneric("ALL.p.value<-", function(x, value) standardGeneric("ALL.p.value<-"))
+setMethod("ALL.p.value", "CorrelatedSNPs", function(x) x@ALL.p.value)
+setReplaceMethod("ALL.p.value", "CorrelatedSNPs", function(x, value) {
+                 x@ALL.p.value <- value
+                 x
+         })
+setGeneric("AFR.p.value", function(x) standardGeneric("AFR.p.value"))
+setGeneric("AFR.p.value<-", function(x, value) standardGeneric("AFR.p.value<-"))
+setMethod("AFR.p.value", "CorrelatedSNPs", function(x) x@AFR.p.value)
+setReplaceMethod("AFR.p.value", "CorrelatedSNPs", function(x, value) {
+                 x@AFR.p.value <- value
+                 x
+         })
+setGeneric("AMR.p.value", function(x) standardGeneric("AMR.p.value"))
+setGeneric("AMR.p.value<-", function(x, value) standardGeneric("AMR.p.value<-"))
+setMethod("AMR.p.value", "CorrelatedSNPs", function(x) x@AMR.p.value)
+setReplaceMethod("AMR.p.value", "CorrelatedSNPs", function(x, value) {
+                 x@AMR.p.value <- value
+                 x
+         })
+setGeneric("ASN.p.value", function(x) standardGeneric("ASN.p.value"))
+setGeneric("ASN.p.value<-", function(x, value) standardGeneric("ASN.p.value<-"))
+setMethod("ASN.p.value", "CorrelatedSNPs", function(x) x@ASN.p.value)
+setReplaceMethod("ASN.p.value", "CorrelatedSNPs", function(x, value) {
+                 x@ASN.p.value <- value
+                 x
+         })
+setGeneric("EUR.p.value", function(x) standardGeneric("EUR.p.value"))
+setGeneric("EUR.p.value<-", function(x, value) standardGeneric("EUR.p.value<-"))
+setMethod("EUR.p.value", "CorrelatedSNPs", function(x) x@EUR.p.value)
+setReplaceMethod("EUR.p.value", "CorrelatedSNPs", function(x, value) {
+                 x@EUR.p.value <- value
+                 x
+         })
+setGeneric("ALL.overlapping.snps.geno", function(x) standardGeneric("ALL.overlapping.snps.geno"))
+setGeneric("ALL.overlapping.snps.geno<-", function(x, value) standardGeneric("ALL.overlapping.snps.geno<-"))
+setMethod("ALL.overlapping.snps.geno", "CorrelatedSNPs", function(x) x@ALL.overlapping.snps.geno)
+setReplaceMethod("ALL.overlapping.snps.geno", "CorrelatedSNPs", function(x, value) {
+                 x@ALL.overlapping.snps.geno <- value
+                 x
+         })
+setGeneric("AFR.overlapping.snps.geno", function(x) standardGeneric("AFR.overlapping.snps.geno"))
+setGeneric("AFR.overlapping.snps.geno<-", function(x, value) standardGeneric("AFR.overlapping.snps.geno<-"))
+setMethod("AFR.overlapping.snps.geno", "CorrelatedSNPs", function(x) x@AFR.overlapping.snps.geno)
+setReplaceMethod("AFR.overlapping.snps.geno", "CorrelatedSNPs", function(x, value) {
+                 x@AFR.overlapping.snps.geno <- value
+                 x
+         })
+setGeneric("AMR.overlapping.snps.geno", function(x) standardGeneric("AMR.overlapping.snps.geno"))
+setGeneric("AMR.overlapping.snps.geno<-", function(x, value) standardGeneric("AMR.overlapping.snps.geno<-"))
+setMethod("AMR.overlapping.snps.geno", "CorrelatedSNPs", function(x) x@AMR.overlapping.snps.geno)
+setReplaceMethod("AMR.overlapping.snps.geno", "CorrelatedSNPs", function(x, value) {
+                 x@AMR.overlapping.snps.geno <- value
+                 x
+         })
+setGeneric("ASN.overlapping.snps.geno", function(x) standardGeneric("ASN.overlapping.snps.geno"))
+setGeneric("ASN.overlapping.snps.geno<-", function(x, value) standardGeneric("ASN.overlapping.snps.geno<-"))
+setMethod("ASN.overlapping.snps.geno", "CorrelatedSNPs", function(x) x@ASN.overlapping.snps.geno)
+setReplaceMethod("ASN.overlapping.snps.geno", "CorrelatedSNPs", function(x, value) {
+                 x@ASN.overlapping.snps.geno <- value
+                 x
+         })
+setGeneric("EUR.overlapping.snps.geno", function(x) standardGeneric("EUR.overlapping.snps.geno"))
+setGeneric("EUR.overlapping.snps.geno<-", function(x, value) standardGeneric("EUR.overlapping.snps.geno<-"))
+setMethod("EUR.overlapping.snps.geno", "CorrelatedSNPs", function(x) x@EUR.overlapping.snps.geno)
+setReplaceMethod("EUR.overlapping.snps.geno", "CorrelatedSNPs", function(x, value) {
+                 x@EUR.overlapping.snps.geno <- value
+                 x
+         })
+
+setMethod("show",
+          signature=signature(object="TagSNP"),
+          function(object){
+            cat("class:", class(object), "\n")
+            cat("Tag SNP: ", snpid(object), "\n")
+            if(dim(elementMetadata(overlapping.features(correlated.snps(object))))[1] > 0) {
+            cat(length(unique(names(overlapping.features(correlated.snps(object))))), 
+            "Surrounding SNPs are overlapping: \n")
+            cat(unique(elementMetadata(overlapping.features(correlated.snps(object)))[, "feature"]), "\n")
+            } else {
+              cat("No Surrounding SNPs are overlapping any biofeatures\n")
+            }
+          })
+#setMethod("summary", "TagSNP", function(object) {
+#          data.frame(chromosome=chromosome(object), position=position(object),
+#                     snpid=snpid(object), population=population(object),
+#                     ref.allele=ref.allele(object), alt.allele=alt.allele(object),
+#                     overlapping.features=lapply(overlapping.features(object), function(x) length(x)),
+#                     genotype=genotype(object), R.squared.CorrelatedSNPs=R.squared.CorrelatedSNPs(object),
+#                     D.prime.CorrelatedSNPs=D.prime.CorrelatedSNPs(object))
+#         }
+#)
